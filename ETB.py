1d4424675f07171f418c252bcde2f303b687c837
Cornelius-Figgle <max@fullimage.net>
2022-09-18 15:07:12 +0100

    now supports int, float, bool + str in input

--- ETB.py
+++ ETB.py
@@ -12,31 +12,48 @@ class etb_handle():
         str_cmd = self.eng_cmd.strip().split(' ', num_of_params)
 
         match str_cmd[0]:
-            case 'set': # `set x to 4`
+            case 'set':
                 var_to_set = str_cmd[1]
                 value_to_set = str_cmd[3]
+
+                if value_to_set.isnumeric(): #num
+                    if value_to_set.isdigit():
+                        value_to_set = int(value_to_set)
+                    else: 
+                        value_to_set = float(value_to_set)
+
+                elif value_to_set == 'true': #bool
+                    value_to_set = True
+                elif value_to_set == 'false': #bool
+                    value_to_set = False
+                elif value_to_set == 'none': #bool
+                    value_to_set = None
+
+                else: #str
+                    value_to_set = self.text_handler(value_to_set)
 
                 self.user_vars[var_to_set] = value_to_set
 
                 return True
-            case 'display': # `display x`
-                try: 
-                    print(self.user_vars[''.join(str_cmd[1:])])
-                except KeyError: 
-                    text = ' '.join(str_cmd[1:])
-                    text = text.replace(
-                        r' newline ', '\n').replace(
-                            r' tab ', '\t').replace(
-                                r' reset caret ', '\r').replace(
-                                    r'\\', '\\')
-                    for var in self.user_vars.keys():
-                        text = text.replace(f'variable {var}', f'{self.user_vars[f"{var}"]}')
-                        
-                    print(text)
+            case 'display':
+                text = ' '.join(str_cmd[1:])
+                print(self.text_handler(text))
 
                 return True
             case _:
                 return False
+
+    def text_handler(self, text: str) -> str:
+        text = text.replace(
+            r' newline ', '\n').replace(
+                r' tab ', '\t').replace(
+                    r' reset caret ', '\r').replace(
+                        r'\\', '\\')
+        for var in self.user_vars.keys():
+            text = text.replace(f'variable {var}', f'{self.user_vars[f"{var}"]}')
+
+        return text
+
 
 #________________________________________________________________________________________________________________________________
 
