da2883d348e3244b26b7356efe2fb11e0c50c0d9
Cornelius-Figgle <max@fullimage.net>
2022-09-18 12:07:37 +0100

    moved to single file

--- ETB.py
+++ ETB.py
@@ -1,11 +1,17 @@
-from . import interactive_shell as sh
+
 
 #________________________________________________________________________________________________________________________________
 
-def determine_cmd_action():
+def input_cycle():
+    eng_cmd = input('> ')
+    determine_cmd_action(eng_cmd)
+
+def main():
     print('Hello World!\n')
+
+    input_cycle()
 
 #________________________________________________________________________________________________________________________________
 
 if __name__ == '__main__': 
-    sh.main()
+    main()
