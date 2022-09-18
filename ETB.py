e414684dce09a7bd2fa0545d515b9553621bb34b
Cornelius-Figgle <max@fullimage.net>
2022-09-18 12:17:29 +0100

    created class init'er

--- ETB.py
+++ ETB.py
@@ -2,14 +2,29 @@
 
 #________________________________________________________________________________________________________________________________
 
-def input_cycle():
-    eng_cmd = input('> ')
-    determine_cmd_action(eng_cmd)
+class etb_handle():
+    def __init__(self, eng_cmd: str) -> None:
+        self.eng_cmd = str(eng_cmd)
 
-def main():
+    def decode_start(self) -> str:
+        return self.eng_cmd.split(' ', 0)
+
+#________________________________________________________________________________________________________________________________
+
+def input_cycle() -> None:
+    eng_cmd = input('> ')
+
+    p = etb_handle(eng_cmd)
+    p.decode_start()
+
+def main() -> None:
     print('Hello World!\n')
 
-    input_cycle()
+    while True: 
+        try: 
+            input_cycle()
+        except KeyboardInterrupt:
+            break
 
 #________________________________________________________________________________________________________________________________
 
